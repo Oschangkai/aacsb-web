259261524620b38782cac329b7beb66bb7d58426
oschangkai <oschangkai@kaiyeee.tw>
2021-04-27 18:32:18 +0800

    Implement: role detail

--- src/app/modules/main-system/pages/role-page/role-page.component.ts
+++ src/app/modules/main-system/pages/role-page/role-page.component.ts
@@ -4,19 +4,19 @@ import { Permission } from '@model/ApplicationPermission.model';
 import {ActivatedRoute} from '@angular/router';
 import {NgProgress} from 'ngx-progressbar';
 import {Subscription} from 'rxjs';
-import {Roles} from '@model/query.response.model';
+import {Role, Roles} from '@model/query.response.model';
 import {ResponseData} from '@model/response.model';
 import {SystemService} from '@module/main-system/service/system.service';
 
 @Component({
   selector: 'app-role-page',
-  templateUrl: './role-page.component.html',
-  providers: [SystemService]
+  templateUrl: './role-page.component.html'
 })
 export class RolePageComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
+    private systemService: SystemService,
     private ngProgress: NgProgress
   ) {
     this.httpStateSubscription = this.ngProgress.ref('http-load').state.subscribe(state => {
@@ -32,6 +32,17 @@ export class RolePageComponent implements OnInit, OnDestroy {
 
   // data
   roles: ResponseData<Roles> = { data: [], count: 0 };
+  roleDetail: {[x: string]: Role} = { '000': {id: '', name: '', claims: []} };
+
+  onDetailOpen(open: boolean, role: Roles | null): void {
+    // If closed, first loaded, or loaded before
+    // not processing this function
+    if (!open || role === null || this.roleDetail[role.id]) { return; }
+
+    this.systemService.getRole(role.id).subscribe(response => {
+      this.roleDetail[role.id] = response.data as Role;
+    });
+  }
 
   ngOnInit(): void {
     this.route.data.subscribe(
@@ -40,6 +51,13 @@ export class RolePageComponent implements OnInit, OnDestroy {
         this.roles.count = response.count;
       }
     );
+  }
+
+  load(): void {
+    this.systemService.getRoles().subscribe(response => {
+      this.roles.data = [...response.data as Roles[]];
+      this.roles.count = response.count;
+    });
   }
   ngOnDestroy(): void {
     this.httpStateSubscription.unsubscribe();
