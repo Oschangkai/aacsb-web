b2076f86252bcace21912ee5226d378e98e23416
oschangkai <oschangkai@kaiyeee.tw>
2021-08-20 15:06:54 +0800

    Test: fire wake sql after app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InitializerService } from '@service/initializer.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MAYO Board';
+export class AppComponent implements OnInit{
+  title = 'MAYO Boardroom';
+
+  constructor(private init: InitializerService) { }
+
+  ngOnInit(): void {
+    this.init.wakeUpSql().subscribe();
+  }
 }
