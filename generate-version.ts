43a0a81bdc52e8dd6abd72a4e2d56447a97cc5b4
Oschangkai <oschangkai@kaiyeee.tw>
2023-06-13 15:09:18 +0800

    Feat: update generate version script

--- generate-version.ts
+++ generate-version.ts
@@ -1,8 +1,15 @@
 const file = require('fs');
 
+const revisionDate = require('child_process')
+  .execSync("echo $(git show -s --date=format:'%Y%m%d-%H%M' --format=%cd)")
+  .toString().trim();
+const commitHash = require('child_process')
+  .execSync('echo $(git rev-parse --short HEAD)')
+  .toString().trim();
+
 const targetPath = './src/app/shared/components/app-version/app-version.component.html';
 const envConfigFile = `<span style="color: rgb(236, 236, 236); font-size: 12px; cursor: default;">
-  ${process.env.version}
+  ${revisionDate}-${commitHash}
 </span>
 `;
 
